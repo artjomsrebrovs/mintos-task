26ee57875c40d60dd2d09ca1438195af32e660ac
artjomsrebrovs <artjoms_rebrovs@inbox.lv>
2023-03-12 16:05:25 +0200

    tests

---
+++ src/test/java/com/mintos/task/client/Ip2Loc/impl/Ip2LocClientImplTest.java
@@ -0,0 +1,12 @@
+package com.mintos.task.client.Ip2Loc.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ip2LocClientImplTest {
+
+    @Test
+    void getLocationData() {
+    }
+}
